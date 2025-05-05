8c3948ccb2d4a7969489d3bf3d7e6b1b084250c2
Patrick Serrano <patrickcmserrano@gmail.com>
2025-05-05 01:06:12 -0300

    Adiciona comentário sobre a configuração do Vite e mantém a base relativa

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     svelte(),
   ],
+  base: './', // Isso garante que os caminhos sejam relativos, funcionando em qualquer servidor
 });
